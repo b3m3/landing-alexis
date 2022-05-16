fc831fd3b080c9930580ba5144515f1905dead34
b3m3 <b3m3@yandex.ru>
2022-05-16 13:06:18 +0300

    fix hrader

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,6 @@
 import webp from './lib/webp.js';
 import burger from './lib/burger.js';
+import fixedHeader from './lib/fixedHeader.js';
 import playVideo from './modules/play-video.js';
 import {tabs, createDots} from './modules/tabs.js';
 
@@ -8,6 +9,7 @@ document.addEventListener('DOMContentLoaded', () => {
   
   webp();
   burger(768);
+  fixedHeader(50);
   playVideo();
   tabs('.card-team', '.info-team__item', '.info-team');
   createDots();
